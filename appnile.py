39f6a45e2ba9bcd82ef36eb12d4726cd97a6e644
Sanjog Gururaj <54097885+sanjogururaj47@users.noreply.github.com>
2022-02-26 18:42:39 -0600

    Driver Script

--- appnile.py
+++ appnile.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 os.system("echo Pulling from postgres...")
-os.system("heroku pg:psql postgresql-dimensional-74388 --app appnile --command "COPY (player) TO STDOUT WITH CSV" > file.csv")
+os.system("heroku pg:psql postgresql-dimensional-74388 --app appnile --command \"COPY (player) TO STDOUT WITH CSV\" > file.csv")
 os.system("echo Done!")
 
 df = pd.read_csv("file.csv")
