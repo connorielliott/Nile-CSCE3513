9091c5e7ea02fb856dd6ce569d044d3fb9c3f204
Rithyka Heng <rkheng@uark.edu>
2022-03-03 16:01:52 -0500

    remove pre-send message check

--- GUI/front/js/comms.js
+++ GUI/front/js/comms.js
@@ -1,8 +1,6 @@
 const socket = io("http://127.0.0.1:8000");
 
 function send(data) {
-	if(typeof data == "object") data = JSON.stringify(data);
-	else data = data.toString();
 	socket.emit("data", data);
 	console.log("[.->n]\t" + data);
 }
