0ae003bcf7d7d7468424dda92a05f72d2a00d4d1
benton <bentonandersonwps@gmail.com>
2022-03-13 21:47:21 -0500

    fixed python two array code

--- two_arrays.py
+++ two_arrays.py
@@ -1,25 +1,36 @@
 def two_arrays(str):
-    array_switch = True
+    array_switch = False
+    change_array_one = True
+    tempstring = ""
     temparray1 = []
     temparray2 = []
+    index = 0
     for s in str:
+        stop_int = len(str)
         if (s == ':') or (s == ','):
-            array_switch = not array_switch
+            array_switch = True
+        elif (index + 1 == stop_int):
+            array_switch = True
+            tempstring = tempstring + s
+        elif s != ' ':
+            tempstring = tempstring + s
         if array_switch:
-            if (s == ':') or (s == ',') or (s == ' '):
-                pass
+            if change_array_one:
+                temparray1.append(tempstring)
+                tempstring = ""
+                change_array_one = False
+                array_switch = False
             else:
-                temparray1.append(s)
-        else:
-            if (s == ':') or (s == ',') or (s == ' '):
-                pass
-            else:
-                temparray2.append(s)
+                temparray2.append(tempstring)
+                tempstring = ""
+                change_array_one = True
+                array_switch = False
+        index += 1
 
     return temparray1, temparray2
 
-# code to test above
-# str = "a:1, b:2, c:3"
+# test code
+# str = "abc:123, def:456, ghi:789"
 # array1 = []
 # array2 = []
 # array1, array2 = two_arrays(str)
